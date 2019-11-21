d5851c811a06b6d662a54eae5be06706f6f93126
MSmyth97 <mathew.smyth@outlook.com>
2019-11-21 13:35:23 +0000

    module4

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
-const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const app = express();
 const {accounts, users, writeJSON} = require('./data');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -16,29 +18,11 @@ app.use(express.urlencoded({extended: true}));
 
 
 app.get('/', (req, res) => res.render('index', {title:'Account Summary', accounts:accounts}));
-app.get('/savings', (req, res) => res.render('account', {account: accounts.savings}));
-app.get('/credit', (req, res) => res.render('account', {account: accounts.credit}));
-app.get('/checking', (req, res) => res.render('account', {account: accounts.checking}));
-app.get('/transfer', (req, res) => res.render('account', {account: accounts.transfer}));
-app.get('/payment', (req, res) => res.render('payment', {account: accounts.credit}));
-app.post('/payment', (req, res) => {
-   accounts.credit.balance -= req.body.amount;
-   accounts.credit.available += parseInt(req.body.amount);
-   writeJSON();
-   res.render('payment', {account: accounts.credit});
 
-});
-
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance = accounts[req.body.from].balance - req.body.amount;
-    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount, 10);
-    //const accountsJSON = JSON.stringify(accounts, null, 4);
-    //fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
-    writeJSON();
-    res.render('transfer', {message: "Transfer Completed"});
+app.use('/services', servicesRoutes);
+app.use('/accounts', accountRoutes);
 
 
-});
 
 app.get('/profile', (req, res) => res.render('profile', {user:users[0]}));
 
